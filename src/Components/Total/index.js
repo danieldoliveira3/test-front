49ea4d5ebc28dce23f5422bfd214ab9eda2fd0b2
Daniel Carvalho <dco3@cin.ufpe.br>
2020-05-23 05:04:55 -0300

    total component added

--- src/Components/Total/index.js
+++ src/Components/Total/index.js
@@ -2,29 +2,46 @@ import React from "react";
 
 function Total(props) {
   return (
-    <div>
-      <h2>
-        Total:{" "}
-        {props.loaded ? (
-          (
-            props.result.subTotal -
-            props.result.discount +
-            props.result.shippingTotal
-          ).toFixed(2)
-        ) : (
-          <div>carregando</div>
-        )}
-      </h2>
-      <h2>
-        Desconto: {props.loaded ? props.result.discount : <div>carregando</div>}
-      </h2>
-      <h2>
-        Entrega:{" "}
-        {props.loaded ? props.result.shippingTotal : <div>carregando</div>}
-      </h2>
-      <h2>
-        Subtotal: {props.loaded ? props.result.subTotal : <div>carregando</div>}
-      </h2>
+    <div className="totalWrap">
+      <div className="totalBox">
+        <div className="totalFlexbox">
+          <div>PRODUTOS: </div>
+          <div>
+            R$ {props.loaded ? props.result.subTotal : <div>carregando</div>}
+          </div>
+        </div>
+
+        <div className="totalFlexbox">
+          <div>FRETE: </div>
+          <div>
+            R${" "}
+            {props.loaded ? props.result.shippingTotal : <div>carregando</div>}
+          </div>
+        </div>
+
+        <div className="totalFlexbox orange">
+          <div>DESCONTO: </div>
+          <div>
+            - R$ {props.loaded ? props.result.discount : <div>carregando</div>}
+          </div>
+        </div>
+        <div className="totalFlexbox totalPrice">
+          <div>TOTAL</div>
+
+          <div>
+            R${" "}
+            {props.loaded ? (
+              (
+                props.result.subTotal -
+                props.result.discount +
+                props.result.shippingTotal
+              ).toFixed(2)
+            ) : (
+              <div>carregando</div>
+            )}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
